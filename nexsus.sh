dae5fb4eb1b31e33b6a73d94e017590e8827b955
Dhiman23 <sajaldhiman68@gmail.com>
2024-04-13 02:15:30 +0530

    code

--- nexsus.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update
-
-sudo apt install docker.io -y 
-sudo chmod 666 /var/run/docker.sock 
-
-sudo docker run -d --name nexsus -p 8081:8081 sonatype/nexus3
--- sonar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update
-
-sudo apt install docker.io -y 
-sudo chmod 666 /var/run/docker.sock 
-
-sudo docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
